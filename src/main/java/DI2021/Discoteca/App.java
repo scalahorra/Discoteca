57891a5139e11448b02284ca2139d0c2ef441128
darioaxel <darioaxel@komoi>
2020-12-01 18:15:35 +0100

    Added services to get objects from the json files and basic example on App showing data

--- src/main/java/DI2021/Discoteca/App.java
+++ src/main/java/DI2021/Discoteca/App.java
@@ -1,5 +1,10 @@
 package DI2021.Discoteca;
 
+import java.util.List;
+
+import DI2021.Discoteca.models.Album;
+import DI2021.Discoteca.services.AlbumRecoveryService;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        List<Album> myAlbums = AlbumRecoveryService.getAllAlbums();
+        for(var album: myAlbums) {
+        	System.out.println(album);
+        }
     }
 }
