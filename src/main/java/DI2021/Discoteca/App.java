4e44a03ec804ffe84adb39d97da3e924eccb49ec
darioaxel <darioaxel@komoi>
2020-12-01 17:10:52 +0100

    Java project structure working

---
+++ src/main/java/DI2021/Discoteca/App.java
@@ -0,0 +1,13 @@
+package DI2021.Discoteca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
